a777448c154660e7078006491ed13b502a19d8a7
David Federlein <david.federlein@eucalyptus.com>
2012-07-13 15:35:00 -0500

    Added Functionality
    Cleanup loopback devices

--- virshcleanup.sh
+++ virshcleanup.sh
@@ -8,7 +8,17 @@ for v in `echo ${ALL[@]}`; do
             TSDATE=($(date +%H%M%S.%m%d%Y))
             echo $TSDATE "Detected bad instance, destroying domain: " ${x} " instance id: "${v}
             virst destroy ${x}
+            for d in `dmsetup table | grep ${v} | cut -f 1 -d ':'`; do
+                dmsetup remove ${d}; sleep 5; dmsetup remove ${d};
             done
+            for l in `dmsetup table | grep ${v} | cut -f 1 -d ':' | sed "s/^.*${v}-//"`; do
+                WID=($(dmsetup table | grep ${v} | cut -f 1 -d ':'| sed "s/\-/ /g" | awk '{print $2}' | sort -u))
+                for LOOP in `losetup -j /var/lib/eucalyptus/instances/work/${WID}/${l}.blocks 2> /dev/null | awk -F":" '{print $1}'`; do
+                    losetup -d $LOOP
+                done
+#                losetup -j ${l}.loopback
+            done
+        done
     else
         echo "domain: " ${x} " instance id: "${v} "is good.  Leaving alone."
     fi
