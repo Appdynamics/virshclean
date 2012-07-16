608b43353f6d54d16be4e48256c2ba121ea9757f
William Jimenez <wjimenez5271@gmail.com>
2012-07-16 10:39:39 -0700

    Added logging of the script's activity to a text file in addition to stdout.

--- virshcleanup.sh
+++ virshcleanup.sh
@@ -6,13 +6,14 @@ for v in `echo ${ALL[@]}`; do
         TSDATE=($(date +%H%M%S.%m%d%Y))
         for x in `virsh list | grep "i-" | grep ${v} | awk '{print $1}'`; do
             TSDATE=($(date +%H%M%S.%m%d%Y))
-            echo $TSDATE "Detected bad instance, destroying domain: " ${x} " instance id: "${v}
+            echo $TSDATE "Detected bad instance, destroying domain: " ${x} " instance id: "${v} | tee -a /tmp/virshclean.log
             virsh destroy ${x}
         done
         for l in `dmsetup table | grep ${v} | cut -f 1 -d ':'`; do
             dmsetup remove  $l; sleep 2; dmsetup remove $l
         done
     else
-        echo "domain: " ${x} " instance id: "${v} "is good.  Leaving alone."
+        TSDATE=($(date +%H%M%S.%m%d%Y))
+	echo $TSDATE "domain: " ${x} " instance id: "${v} "is good.  Leaving alone." | tee -a /tmp/virshclean.log
     fi
 done
